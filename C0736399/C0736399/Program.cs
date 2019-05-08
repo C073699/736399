46357fb3dda686f33aff95c445a6c6d15592c126
Gurinderpal Kaur <736399@student.tor.loc>
2019-05-08 11:38:41 -0400

    Task B Done

--- C0736399/C0736399/Program.cs
+++ C0736399/C0736399/Program.cs
@@ -10,7 +10,26 @@ namespace C0736399
     {
         static void Main(string[] args)
         {
+            Method m = new Method();
+            m.Output();
 
+            Console.ReadKey();
+        }
+    }
+
+    class Method
+    {
+        public void Output()
+        {
+            int start = 1098;
+            int sum = 0;
+            while (start >= 514)
+            {
+                Console.WriteLine(start);
+                sum = sum + start;
+                start =start-33;
+            }
+            Console.WriteLine("Sum is " + sum);
         }
     }
 }
